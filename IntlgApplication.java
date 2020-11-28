e7308e9236ccaa8b87f95c3dc4a0f92e65b18589
megha9727 <meghamohan.8888@gmail.com>
2020-11-29 00:10:00 +0530

    v1

---
+++ IntlgApplication.java
@@ -0,0 +1,13 @@
+package project.pocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntlgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntlgApplication.class, args);
+	}
+
+}
