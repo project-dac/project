6c7a2a6efbaf28fe4889e5bc94553b0bfda38974
megha9727 <meghamohan.8888@gmail.com>
2020-11-29 00:13:15 +0530

    v1

---
+++ OrderRepository.java
@@ -0,0 +1,16 @@
+package project.pocs;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository  extends JpaRepository<Orders,String>{
+
+	@Query("from Orders where orderqty >:x")
+	List<Orders> oncat(@Param("x") int y);
+}
+
